3478737b9735f0541ab19143558ffaa80153f9eb
Zofia Luther <zofialuther@Zofias-MacBook-Pro.local>
2023-11-08 06:52:04 -0600

    Fixed eval script

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,8 +1,15 @@
 from calculate_length import *
 from calculate_bleu import *
 from calculate_syntax import *
+import json
 
 if __name__ == "__main__":
     report_length()
     report_syntax()
     report_bleu()
+
+    data = {'output' : 0}
+    
+    
+    with open('../output.json', "w") as file:
+        json.dump(data, file, indent=4)
